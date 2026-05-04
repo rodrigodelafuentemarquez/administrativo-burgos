af90ab7364fb2111e803fe58670f8ecbc3fa74d1
Rodrigo de la Fuente Márquez <rodrigodelafuentemarquez@gmail.com>
2026-05-04 21:37:35 +0200

    style: alinear interfaz con documentacion Bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import remarkGfm from 'remark-gfm';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 export default defineConfig({
   base: '/oposicion',
   site: 'https://example.github.io',
   markdown: {
     remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            className: ['anchor-link'],
+            ariaLabel: 'Enlace a esta seccion',
+          },
+        },
+      ],
+    ],
   },
 });
