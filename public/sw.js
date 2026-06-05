f4f9523124cf922304a2bf6efa35347f1735e740
Rodrigo de la Fuente Márquez <rodrigodelafuentemarquez@gmail.com>
2026-06-05 19:44:19 +0200

    Add Burgos favicon and app icons

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,12 @@
-const CACHE = 'oposicion-v1';
-const ASSETS = ['/', '/oposicion/', '/oposicion/manifest.webmanifest'];
+const CACHE = 'administrativo-burgos-v1';
+const ASSETS = [
+  '/administrativo-burgos/',
+  '/administrativo-burgos/manifest.webmanifest',
+  '/administrativo-burgos/favicon.ico',
+  '/administrativo-burgos/favicon.png',
+  '/administrativo-burgos/icons/icon-192.png',
+  '/administrativo-burgos/icons/icon-512.png',
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(() => null));
